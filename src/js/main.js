ed8515a2cabbc170ab77435442d7b4b153093684
fenq2 <fenq@ukr.net>
2021-07-04 22:08:41 +0300

    add responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,43 @@
 const infoblockSlider = new Swiper(".infoblock__slider", {
+    slidesPerView: 3,
+    spaceBetween: 15,
     navigation: {
       nextEl: ".infoblock__next-btn",
       prevEl: ".infoblock__prev-btn",
     },
+    pagination: {
+         el: ".infoblock__pagination",
+    },
+    breakpoints: {
+      1000: {
+        spaceBetween: 20,
+      },
+      680: {
+        spaceBetween: 15,
+      },
+    }
  });
 
 const yandexSlider = new Swiper(".yandex-review__slider", {
-    slidesPerView: "auto",
+    slidesPerView: 1,
     spaceBetween: 93,
      navigation: {
        nextEl: ".yandex-review__next",
        prevEl: ".yandex-review__prev",
      },
+     pagination: {
+          el: ".yandex-review__pagination",
+     },
+     breakpoints: {
+       1400: {
+         spaceBetween: 93,
+         slidesPerView: "auto",
+       },
+       680: {
+         spaceBetween: 46,
+         slidesPerView: 2,
+       },
+     }
   });
 
 var swiper = new Swiper(".card-slider__thumb", {
@@ -20,7 +46,11 @@ var swiper = new Swiper(".card-slider__thumb", {
     freeMode: true,
     watchSlidesVisibility: true,
     watchSlidesProgress: true,
-    direction: 'vertical',
+    breakpoints: {
+        1600: {
+          direction: 'vertical',
+    }
+  }
 });
 
 var swiper2 = new Swiper(".card-slider__main", {
@@ -80,5 +110,184 @@ const toggleCatalogMenu = function () {
     })
 };
 
+const searchHandler = function () {
+    let searchBtn = document.querySelector('.js-search-open');
+    let searchBlock = document.querySelector('.search-prev')
+
+    searchBtn.addEventListener('click', () => {
+        searchBlock.classList.toggle('search-prev--active');
+    });
+};
+
+const sandwich = function () {
+    let sandwichButton = document.querySelector('.sandwich__open-btn');
+    let sandwichClose = document.querySelector('.sandwich__close-btn');
+    let content = document.querySelector('.sandwich__content');
+
+    sandwichButton.addEventListener('click', function () {
+      content.classList.add('sandwich__content--opened');
+      document.body.style.overflow = 'hidden';
+    });
+
+    sandwichClose.addEventListener('click', function () {
+        content.classList.remove('sandwich__content--opened');
+        document.body.style.overflow = 'visible';
+    });
+};
+
+
+sandwich();
 tab();
 toggleCatalogMenu();
+searchHandler();
+
+
+(function () {
+    'use strict';
+    // breakpoint where swiper will be destroyed
+    // and switches to a dual-column layout
+    const breakpoint = window.matchMedia('(min-width:1000px)');
+    // keep track of swiper instances to destroy later
+    let mySwiper;
+    const breakpointChecker = function () {
+        // if larger viewport and multi-row layout needed
+        if (breakpoint.matches === true) {
+            // clean up old instances and inline styles when available
+            if (mySwiper !== undefined) mySwiper.destroy(true, true);
+            // or/and do nothing
+            return;
+            // else if a small viewport and single column layout needed
+        } else if (breakpoint.matches === false) {
+            // fire small viewport version of swiper
+            return sliderInstagram();
+        }
+    };
+
+    if (window.innerWidth <= 1000 && mySwiper) {
+        mySwiper.destroy(false)
+    }
+
+    const sliderInstagram = function () {
+        mySwiper = new Swiper('.similar-cards__list', {
+            spaceBetween: 45,
+            slidesPerView: 1,
+            navigation: {
+              nextEl: ".similar-cards__next",
+              prevEl: ".similar-cards__prev",
+            },
+            pagination: {
+                el: ".similar-cards__pagination"
+            },
+            breakpoints: {
+              720: {
+                  spaceBetween: 25,
+                slidesPerView: 3,
+              }
+            }
+        });
+    };
+    // keep an eye on viewport size changes
+    breakpoint.addListener(breakpointChecker);
+    // kickstart
+    breakpointChecker();
+})();
+
+(function () {
+    'use strict';
+    // breakpoint where swiper will be destroyed
+    // and switches to a dual-column layout
+    const breakpoint = window.matchMedia('(min-width:1000px)');
+    // keep track of swiper instances to destroy later
+    let mySwiper;
+    const breakpointChecker = function () {
+        // if larger viewport and multi-row layout needed
+        if (breakpoint.matches === true) {
+            // clean up old instances and inline styles when available
+            if (mySwiper !== undefined) mySwiper.destroy(true, true);
+            // or/and do nothing
+            return;
+            // else if a small viewport and single column layout needed
+        } else if (breakpoint.matches === false) {
+            // fire small viewport version of swiper
+            return sliderInstagram();
+        }
+    };
+
+    if (window.innerWidth <= 1000 && mySwiper) {
+        mySwiper.destroy(false)
+    }
+
+    const sliderInstagram = function () {
+        mySwiper = new Swiper('.past-cards__list', {
+            spaceBetween: 45,
+            slidesPerView: 1,
+            navigation: {
+              nextEl: ".past-cards__next",
+              prevEl: ".past-cards__prev",
+            },
+            pagination: {
+                el: ".past-cards__pagination"
+            },
+            breakpoints: {
+              720: {
+                  spaceBetween: 25,
+                slidesPerView: 3,
+              }
+            }
+        });
+    };
+    // keep an eye on viewport size changes
+    breakpoint.addListener(breakpointChecker);
+    // kickstart
+    breakpointChecker();
+})();
+
+(function () {
+    'use strict';
+    // breakpoint where swiper will be destroyed
+    // and switches to a dual-column layout
+    const breakpoint = window.matchMedia('(min-width:1000px)');
+    // keep track of swiper instances to destroy later
+    let mySwiper;
+    const breakpointChecker = function () {
+        // if larger viewport and multi-row layout needed
+        if (breakpoint.matches === true) {
+            // clean up old instances and inline styles when available
+            if (mySwiper !== undefined) mySwiper.destroy(true, true);
+            // or/and do nothing
+            return;
+            // else if a small viewport and single column layout needed
+        } else if (breakpoint.matches === false) {
+            // fire small viewport version of swiper
+            return sliderInstagram();
+        }
+    };
+
+    if (window.innerWidth <= 1000 && mySwiper) {
+        mySwiper.destroy(false)
+    }
+
+    const sliderInstagram = function () {
+        mySwiper = new Swiper('.with-cards__list', {
+            spaceBetween: 45,
+            slidesPerView: 1,
+            navigation: {
+              nextEl: ".with-cards__next",
+              prevEl: ".with-cards__prev",
+            },
+            pagination: {
+                el: ".with-cards__pagination"
+            },
+            breakpoints: {
+              720: {
+                  spaceBetween: 25,
+                slidesPerView: 3,
+              }
+            }
+        });
+    };
+    // keep an eye on viewport size changes
+    breakpoint.addListener(breakpointChecker);
+    // kickstart
+    breakpointChecker();
+})();
